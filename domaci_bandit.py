fbfb54b12c6601a30f6854a9fbeb105bfc6f89ac
Boris <borisculjak@gmail.com>
2023-11-05 13:59:24 +0100

    Dodao sam plotovanje i print mozda interesantnih vrednosti

--- domaci_bandit.py
+++ domaci_bandit.py
@@ -66,6 +66,7 @@ class BanditsEnvironment:
 def choose_greedy_action(q):
     return np.argmax(q)
 
+
 def choose_random_action(n):
     return random.randint(0, n - 1)
 
@@ -98,25 +99,27 @@ def train(bandits_no=5, attempts_no=5000, alpha=0.1, epsilon=0.1, plotting=True)
         plt.scatter(range(len(q)), q, marker=".")
         plt.scatter(range(len(q)), [b.mean for b in env.bandits], marker="x")
 
-    return env.bandits, rewards, q
+    return env, rewards, q
 
 
 # Zadatak 1 : pokrenuti trening za razlicite vrednosti epsilon, prikazati rezultate i izvesti zakljucak o nagibu krive
 def prvi_zadatak():
     plt.figure(figsize=(14, 3))
     for i in range(3):
-        envi, rew, q1 = train(epsilon=10**(-i-1), plotting=False)
-        plot_e(3, i+1, envi, rew)
-        plt.title(10**(-i-1))
+        envi, rew, q1 = train(epsilon=10 ** (-i - 1), plotting=False)
+        plot_e(3, i + 1, envi, rew)
+        plt.title(10 ** (-i - 1))
 
     plt.show()
+
 
 def plot_e(n, i, envi, rew):
     plt.subplot(1, n, i)
     g = np.cumsum(rew)
     max_r = max([b.mean for b in envi])
     plt.plot(g, "r")
-    plt.plot(np.cumsum(max_r * np.ones(len(g))), "b")  
+    plt.plot(np.cumsum(max_r * np.ones(len(g))), "b")
+
 
 # Zakljucak prvog zadatka jeste da bismo najbolje prosli (dobili najvecu kumulativnu nagradu) da smo koristili greedy politiku sve vreme.
 # Prilikom smanjenja vrednosti epsilona, cesce je birana masina sa najvecom srednjom vrednosti (samim tim i veca nagarda) te je nagib
@@ -124,21 +127,79 @@ def plot_e(n, i, envi, rew):
 
 
 # Zadatak 2 : Sa naucenim q i epsilon = 0, pustiti 100 iteracija
-def drugi_zadatak():
-    plt.figure(figsize=(8, 3))
+def drugi_zadatak(plotting=False):
+    env, rewards, q = train(plotting=False)
+    learned_q = q.copy()
 
-    envi, rew, q1 = train(plotting=False)
-    plot_e(2, 1, envi, rew)
-    plt.title("5000 iteracija")
+    # 100 iteracija sa epsilon = 0 (greedy metoda)
+    greedy_rewards = []
+    for _ in range(100):
+        a = choose_greedy_action(learned_q)
+        r = env.take_action(a)
+        greedy_rewards.append(r)
 
-    plt.subplot(1, 2, 2)
-    test_len = 100
-    reward_q1 = [max(q1) for _ in range(test_len)]  # Uvek ce biti ista nagrada
+    avg_rewards_eps_greedy = np.mean(rewards)
+    avg_rewards_greedy = np.mean(greedy_rewards)
+    print(f"Max Q naučena vrednost: {max(learned_q):.2f}")
+    print("Prosečna nagrada - ε-Greedy Politika:", avg_rewards_eps_greedy)
+    print("Prosečna nagrada - Čista Greedy Politika:", avg_rewards_greedy)
+    if plotting:
+        last_100_eps_greedy_rewards = rewards[-100:]
 
-    g1 = np.concatenate((rew, reward_q1))
-    plot_e(2, 2, envi, g1)
-    plt.title("5100 iteracija")
-    plt.show()
+        cumulative_rewards_eps_greedy = np.cumsum(last_100_eps_greedy_rewards)
+        cumulative_rewards_greedy = np.cumsum(greedy_rewards)
+
+        plt.figure(figsize=(12, 6))
+        plt.plot(
+            cumulative_rewards_eps_greedy,
+            label="Kumulativne Nagrade - Poslednjih 100 ε-Greedy",
+        )
+        plt.plot(
+            cumulative_rewards_greedy,
+            label="Kumulativne Nagrade - Greedy",
+            linestyle="--",
+        )
+
+        plt.xlabel("Epoha")
+        plt.ylabel("Kumulativna Nagrada")
+        plt.title("Poredjenje Kumulativnih Nagrada: ε-Greedy vs Greedy")
+        plt.legend()
+        plt.show()
+
+        # Poredjenje na poslednjih 10 nagrada
+        max_mean_reward = max(bandit.mean for bandit in env.bandits)
+        eps_greedy_every_10 = last_100_eps_greedy_rewards[::10]
+        greedy_every_10 = greedy_rewards[::10]
+
+        epochs = range(len(rewards) - 100, len(rewards), 10)
+        plt.scatter(
+            epochs,
+            eps_greedy_every_10,
+            label="ε-Greedy Nagrade (svaka 10ta)",
+            marker="o",
+            alpha=0.7,
+        )
+
+        plt.scatter(
+            epochs,
+            greedy_every_10,
+            label="Greedy Nagrade (svaka 10ta)",
+            marker="x",
+            alpha=0.7,
+        )
+
+        plt.axhline(
+            y=max_mean_reward,
+            color="r",
+            linestyle="--",
+            label="Max Mean Moguća Nagrada",
+        )
+
+        plt.xlabel("Epoha")
+        plt.ylabel("Nagrada")
+        plt.title("Nagrada na 10 epoha za poslednjih 100 ε-Greedy i 100 Greedy")
+        plt.legend()
+        plt.show()
 
 
 # Zadatak 3, pod a) : nakon 4000 iteracija napraviti promenu srednje vrednosti i spanova bandita. Prikazati rezultate.
@@ -146,6 +207,5 @@ def drugi_zadatak():
 # Zadatak 3, pod b) : smisliti algoritam za nasumicnu promenu srednje vrednosti i spanova bandita, posle nekog vremena. Prikazati rezultate.
 
 
-prvi_zadatak()
-drugi_zadatak()
-
+# prvi_zadatak()
+drugi_zadatak(plotting=True)
